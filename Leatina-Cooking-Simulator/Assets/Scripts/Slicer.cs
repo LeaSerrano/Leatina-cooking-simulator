88f5562124be1ba6e6cbea94142acb529237c30d
LeaSerrano <leaserrano1119@gmail.com>
2023-12-14 13:54:14 +0100

    Fin du système de découpe

--- Leatina-Cooking-Simulator/Assets/Scripts/Slicer.cs
+++ Leatina-Cooking-Simulator/Assets/Scripts/Slicer.cs
@@ -1,38 +1,45 @@
 ﻿using UnityEngine;
 using EzySlice;
+using UnityEngine.XR.Interaction.Toolkit;
+
 public class Slicer : MonoBehaviour
 {
     private Material materialAfterSlice;
     public LayerMask sliceMask;
+
     public bool isTouched;
+    public Collider isTouchedCollider;
+
     public Material materialTomato;
     public Material materialBread;
 
 
     private int indexSliceableLayer;
     private string tag;
+    private bool hasSliced;
 
     private void Start() {
         string layoutSliceable = "Sliceable";
         indexSliceableLayer = LayerMask.NameToLayer(layoutSliceable);
+
+        hasSliced = false;
     }
 
     private void Update()
     {
-        if (isTouched == true)
+
+        if (isTouched == true && !hasSliced && isTouchedCollider.gameObject.layer == indexSliceableLayer)
         {
-            isTouched = false;
+                Debug.Log("hasSliced : " + hasSliced);
+                Debug.Log("isTouched : " + isTouched);
 
-            Collider[] objectsToBeSliced = Physics.OverlapBox(transform.position, new Vector3(1, 0.1f, 0.1f), transform.rotation, sliceMask);
-            
-            foreach (Collider objectToBeSliced in objectsToBeSliced)
-            {
-                Debug.Log(objectToBeSliced);
+                isTouched = false;
 
-                Renderer collisionRenderer = objectToBeSliced.GetComponent<Renderer>();
-                tag = objectToBeSliced.tag;
 
-                if (tag == "Tomato") {
+                tag = isTouchedCollider.tag;
+
+                if (tag == "Tomato")
+                {
                     materialAfterSlice = materialTomato;
                 }
                 else if (tag == "Bread")
@@ -40,19 +47,24 @@ public class Slicer : MonoBehaviour
                     materialAfterSlice = materialBread;
                 }
 
-                SlicedHull slicedObject = SliceObject(objectToBeSliced.gameObject, materialAfterSlice);
+                SlicedHull slicedObject = SliceObject(isTouchedCollider.gameObject, materialAfterSlice);
 
-                GameObject upperHullGameobject = slicedObject.CreateUpperHull(objectToBeSliced.gameObject, materialAfterSlice);
-                GameObject lowerHullGameobject = slicedObject.CreateLowerHull(objectToBeSliced.gameObject, materialAfterSlice);
+                GameObject upperHullGameobject = slicedObject.CreateUpperHull(isTouchedCollider.gameObject, materialAfterSlice);
+                GameObject lowerHullGameobject = slicedObject.CreateLowerHull(isTouchedCollider.gameObject, materialAfterSlice);
 
-                upperHullGameobject.transform.position = objectToBeSliced.transform.position;
-                lowerHullGameobject.transform.position = objectToBeSliced.transform.position;
+                upperHullGameobject.transform.position = isTouchedCollider.transform.position;
+                lowerHullGameobject.transform.position = isTouchedCollider.transform.position;
 
                 MakeItPhysical(upperHullGameobject);
                 MakeItPhysical(lowerHullGameobject);
 
-                Destroy(objectToBeSliced.gameObject);
-            }
+                Destroy(isTouchedCollider.gameObject);
+
+                hasSliced = true;
+        }
+        else if (hasSliced && isTouchedCollider.gameObject.layer != indexSliceableLayer)
+        {
+            hasSliced = false;
         }
     }
 
@@ -63,12 +75,13 @@ public class Slicer : MonoBehaviour
         
         obj.layer = indexSliceableLayer;
         obj.tag = tag;
+
+        XRGrabInteractable grabInteractableComponent = obj.AddComponent<XRGrabInteractable>();
     }
 
     private SlicedHull SliceObject(GameObject obj, Material crossSectionMaterial = null)
     {
         return obj.Slice(transform.position, transform.up, crossSectionMaterial);
     }
-
 
 }
