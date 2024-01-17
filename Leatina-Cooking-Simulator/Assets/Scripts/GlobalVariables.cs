1664c50851cfed84c7e06ad127844f4a7d7de4ac
LeaSerrano <leaserrano1119@gmail.com>
2024-01-17 19:26:57 +0100

    Ajout de l'ui avec les nouveaux ingrédients

--- Leatina-Cooking-Simulator/Assets/Scripts/GlobalVariables.cs
+++ Leatina-Cooking-Simulator/Assets/Scripts/GlobalVariables.cs
@@ -7,7 +7,14 @@ public static class GlobalVariables
     public static bool heatOn = false;
     public static bool hotPan = false;
     public static bool validRecipe = false;
-    public static string[] orderList = { "Tomato", "Meat"};
+    public static string[] orderListRecipe1 = { "Tomato", "Meat"};
+    public static string[] firstIngredientOrderListRecipe2 = { "Meat", "Tomato", "Onion", "Mushroom" };
+    public static string[][] secondIngredientOrderListRecipe2 = {
+        new string[] { "Tomato", "Onion", "Mushroom" },
+        new string[] { "Onion", "Mushroom" },
+        new string[] { "Meat", "Tomato" },
+        new string[] { "Meat", "Tomato" }
+    }; 
     public static int actualOrder;
     public static bool shouldChangeRecipe = false;
     public static bool shouldDespawnIngredients = false;
